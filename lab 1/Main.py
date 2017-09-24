0164e5819a278d829728f51fec254ae8c4b24c8c
SeminAlex <semin.aleks@list.ru>
2017-09-24 21:29:22 +0300

    First commit

---
+++ lab 1/Main.py
@@ -0,0 +1,27 @@
+from random import sample
+
+
+def parse_file(filename):
+    with open(filename, "r") as f:
+        lines =f.readlines()
+    dimension = int(lines[0])
+    flow_matrix = [[]] * dimension
+    distance = [[]]*dimension
+    for i in range(dimension):
+        distance[i] = [int(dist) for dist in lines[i+1].split()]
+        flow_matrix[i] = [int(flow) for flow in lines[i+dimension+2].split()]
+    return dimension, distance, flow_matrix
+
+
+def random_solution(dimension):
+    rsolution = range(dimension)
+    return sample(rsolution)
+
+
+def main():
+    print parse_file("instances/tai100a")
+    print "main"
+
+
+if "__main__":
+    main()
---
+++ lab 1/instances/experiment.py
@@ -0,0 +1,7 @@
+from random import shuffle,sample
+
+l = range(30)
+
+print l
+print shuffle(l)
+print sample(l, 30)
