cbe0f9da64a934d50565c306b834c3a4f567d57e
zelenov aleksey <zelenovaleksey@gmail.com>
2017-10-26 23:22:08 +0300

    add

--- Lab 2/Biclustering.py
+++ Lab 2/Biclustering.py
@@ -1,3 +1,8 @@
+from collections import Counter
+from random import sample
+
+from numpy.matlib import rand, random
+
 
 class BiCl:
     __slots__ = ["m", "p", "matrix", "machines", "parts", "max_cluster", "ones_n"]
@@ -82,6 +87,43 @@ class BiCl:
             for j in range(self.p):
                 return
         return
+
+    def devide_cluster(self, candidate):
+        '''
+        devides cluster into 2 separate clusters
+        :param candidate: cluster to devide
+        :return:
+        '''
+        m_indices = [i for i, x in enumerate(self.machines) if x == candidate]
+        p_indices = [i for i, x in enumerate(self.machines) if x == candidate]
+        m_l = int(len(m_indices) / 2)
+        for i in m_indices[:m_l]:
+            self.machines = max(self.machines) + 1
+        p_l = int(len(p_indices) / 2)
+        for i in p_indices[:p_l]:
+            self.parts = max(self.parts) + 1
+
+    def division_neighbourhood(self):
+        '''
+        neigborhood of cluster division
+        :return:
+        '''
+        m_c = Counter(self.machines)
+        p_c = Counter(self.parts)
+        clusters = list[m_c.keys()]
+        candidates = []
+        for i in clusters:
+            if m_c[i] > 1 and p_c > 1 :
+                candidates.append(i)
+        if len(candidates) != 0:
+            candidate = sample(candidates,1)
+
+        self.devide_cluster(candidate)
+
+
+
+
+
 
 bicl = BiCl(0, 0)
 print(bicl.parse_file("instances/testinstance"))
